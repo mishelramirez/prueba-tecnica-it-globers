4eb618eb7402dce2b4a78e5dc6fcf142b07fa3ce
cimirame <topacio2211@hotmail.com>
2022-08-29 16:09:12 -0500

    inicio de proyecto js

---
+++ Desktop/javascript/proyectojs-coderhouse/Proyecto.js
@@ -0,0 +1,19 @@
+let añoActual = parseInt (prompt("año actual"))
+let añoNacimiento = parseInt (prompt("año de nacimiento"))
+let resultadoResta = añoActual - añoNacimiento;
+let nombreUsuario = prompt("ingresa tu nombre")
+   // document.write("tu edad actual es: " + resultadoResta);
+
+        alert("tu edad actual es: " + resultadoResta )
+
+
+        alert("Biernvenido " + nombreUsuario )
+
+            console.log(resultadoResta)
+if(resultadoResta<=18){
+    alert("eres menor de edad"+ resultadoResta)
+}
+ else  (resultadoResta=>18)
+    alert("eres mayor de edad " + resultadoResta + " puedes realizar la compra")
+
+
