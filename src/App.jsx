96ab1d6ae7fad3411d295dc60e2d28b57f8a2a4e
Cintya Ramirez <rmishel564@gmail.com>
2022-09-02 18:18:05 -0500

    se ha creado la estructura de la pagina, y creacios de main.scss

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import Header from "./componentes/Header";
+import Home from "./componentes/Home";
+import Footer from "./componentes/Footer";
+
+function App() {
+  return (
+    <div>
+
+      <Header />
+      <Home />
+      <Footer />
+      
+    </div>
+  );
+}
+
+export default App;
---
+++ src/componentes/Header/index.jsx
@@ -0,0 +1,32 @@
+import React from "react";
+
+export default function Header() {
+  return (
+    <div className="header.contenedor">
+      <div className="header.logo">
+        <p>PUFI</p>
+      </div>
+      <div>
+        <div>
+          
+          <p>PUFI PUFF </p>
+          
+        </div>
+        <div>
+          <p>PUFI RAIN</p>
+        </div>
+        <div>
+          
+          <p>PUFI CART</p>
+        </div>
+        <div>
+          <p>PUFI NAP</p>
+        </div>
+      </div>
+      <div>
+        <p>MI CUENTA</p>
+        <p>MI COMPRA</p>
+      </div>
+    </div>
+  );
+}
