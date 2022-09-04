bde7b5d8c135257c48735192bb33f678ed6fcd47
Cintya Ramirez <rmishel564@gmail.com>
2022-09-04 16:57:25 -0500

    Solucion de error de css

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
-import Header from "./Components/Header";
-import Home from "./Components/Home";
-import Footer from "./Components/Footer";
+import Header from "./components/Header";
+import Home from "./components/Home";
+import Footer from "./components/Footer";
 
 function App() {
   return (
