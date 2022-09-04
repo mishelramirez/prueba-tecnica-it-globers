4e38716fdff796a527f1dd75119201d46903fddb
Cintya Ramirez <rmishel564@gmail.com>
2022-09-03 21:54:56 -0500

    Se realiza footer

--- src/Components/Footer/index.jsx
+++ src/Components/Footer/index.jsx
@@ -1,35 +1,60 @@
-import React from 'react'
-import style from "./_footer.modules.scss"
+import React from "react";
+import style from "./_footer.module.scss";
+import {
+  IoLogoFacebook,
+  IoLogoTwitter,
+  IoLogoInstagram,
+  IoQrCodeOutline,
+  IoShieldHalfSharp,
+} from "react-icons/io5";
 
 export default function Footer() {
   return (
     <div className={style.containerFooter}>
-      <div>Pufi</div>
-      <div className={style.footerUno}>
+      <div> PUFI</div>
+      <div>
         <p>PUFI RAIN</p>
         <p>PUFI PUFF</p>
         <p>PUFI CART</p>
         <p>PUFI NAV</p>
       </div>
-      
+
       <div>
-        <p>CONTACTO</p>
-        <p>AYUDA</p>
-        <p>COMO COMPRAR</p>
-        <p>TERMINOS Y CONDICIONES</p>
+        <div>
+          <p>CONTACTO</p>
+          <p>AYUDA</p>
+          <p>COMO COMPRAR</p>
+          <p>TERMINOS Y CONDICIONES</p>
+        </div>
+      </div>
+      <div>
+        <p>Compra 100% segura</p>
+
+        <div className={style.containerIconShield}>
+
+          
+
+            <div className={style.iconShield}>
+              <IoQrCodeOutline />
+              <IoShieldHalfSharp />
+            </div>
+
+            <div>
+              <p>Compra con <br/>la garantia <br/> de Pufi</p>
+            </div>
+
+          </div>
+
+       
       </div>
 
       <div>
         <p>COMPRA SEGURA</p>
-        icon
-        icon
+        <IoLogoFacebook />
+        <IoLogoTwitter />
+        <IoLogoInstagram />
         <p></p>
-      
-      </div>
-
-      <div>
-
       </div>
     </div>
-  )
+  );
 }
